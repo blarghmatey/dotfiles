dfc5bdd468843cb61fcae265884a240e47756817
Tobias Macey <tmacey@BOS-SC-TMacey.local>
2014-05-01 09:24:10 -0400

    Added functions for setting jedi virtualenv on a project basis automatically. Changed whitespace line column config.

--- home/.emacs.d/functions.el
+++ home/.emacs.d/functions.el
@@ -248,3 +248,31 @@ by using nxml's indentation rules."
 
 (provide 'functions)
 ;;; functions.el ends here
+
+(defun project-directory (buffer-name)
+  "Returns the root directory of the project that contains the
+given buffer. Any directory with a .git or .jedi file/directory
+is considered to be a project root."
+  (interactive)
+  (let ((root-dir (file-name-directory buffer-name)))
+    (while (and root-dir
+                (not (file-exists-p (concat root-dir ".git")))
+                (not (file-exists-p (concat root-dir ".jedi"))))
+      (setq root-dir
+            (if (equal root-dir "/")
+                nil
+              (file-name-directory (directory-file-name root-dir)))))
+    root-dir))
+
+(defun project-name (buffer-name)
+  "Returns the name of the project that contains the given buffer."
+  (let ((root-dir (project-directory buffer-name)))
+    (if root-dir
+        (file-name-nondirectory
+         (directory-file-name root-dir))
+      nil)))
+
+(defun jedi-setup-venv ()
+  "Activates the virtualenv of the current buffer."
+  (let ((project-name (project-name buffer-file-name)))
+    (when project-name (venv-workon project-name))))
